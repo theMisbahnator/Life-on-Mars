898531d9eb995ab64396ffdd18480a964a090ace
Misbah Imtiaz <47953004+theMisbahnator@users.noreply.github.com>
2021-07-26 23:22:15 +0000

    added  command, allows querying by rover or date

--- main.py
+++ main.py
@@ -43,6 +43,24 @@ def composeURLPhotos(apiCall, photoType) :
       url_to_file[url] = [file_name, rover, camera, date_taken]
   return url_to_file
 
+def verfiyParam(*param) :
+  params = []
+  for arg in param :
+    params.append(arg)
+    if len(params) == 2 : 
+      break
+  rover = "perseverance"
+  print(params)
+  url_to_file = {} 
+  if len(param) > 0 and param[0].lower() == "curiosity" :
+    rover = 'curiosity'
+  if len(param) > 1 and bool(datetime.strptime(param[1], "%Y-%m-%d")) == True :
+    url_to_file = composeURLPhotos("https://api.nasa.gov/mars-photos/api/v1/rovers/{}/photos?api_key=4hvbdm3crmOBYueVE4FJSwRG3f1vhZykwNhgrqaW&earth_date={}".format(rover, param[1]), 'photos')
+  else :
+    url_to_file = composeURLPhotos('https://api.nasa.gov/mars-photos/api/v1/rovers/{}/latest_photos?api_key=4hvbdm3crmOBYueVE4FJSwRG3f1vhZykwNhgrqaW'.format(rover), 'latest_photos')
+  return url_to_file
+
+
 
 
 # Client side of the code
@@ -53,12 +71,37 @@ client = commands.Bot(command_prefix = '$')
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-''' 
-$photo : sends up to 3 latest random photos of Mars
+@client.command()
+async def imgTest(ctx, arg1, arg2) :
+  await ctx.send('You passed {} and {}'.format(arg1, arg2))
+
+
+'''
+$img [roverName] []
+sends up to 3 latest random photos of Mars
 to server with rover name, camera type, and date taken 
 '''
 @client.command()
-async def photos(ctx) :
+async def img(ctx, *args) :
+  url_to_file = verfiyParam(*args)
+  if url_to_file == None :
+     await ctx.channel.send("No Photos Available")
+  else :
+       await ctx.channel.send('Here are the latest Photos of Mars!')
+       for urls in url_to_file :
+         file_name = url_to_file[urls][0]
+         urllib.request.urlretrieve(urls, file_name)
+         await ctx.channel.send('_ _')
+         await ctx.channel.send('Rover ' + url_to_file[urls][1] + ' \|| ' + url_to_file[urls][2] + ' || ' + url_to_file[urls][3])
+         await ctx.channel.send(file=discord.File(file_name))
+         os.remove(file_name)
+
+''' 
+$photo : sends up to 3 latest random photos of Mars
+to server with rover name, camera type, and date taken 
+
+@client.command()
+async def img(ctx, rover, date) :
    url_to_file = composeURLPhotos('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=4hvbdm3crmOBYueVE4FJSwRG3f1vhZykwNhgrqaW', 'latest_photos')
    if url_to_file == None :
      await ctx.channel.send("No Photos Available")
@@ -72,7 +115,7 @@ async def photos(ctx) :
          await ctx.channel.send(file=discord.File(file_name))
          os.remove(file_name)
 
-
+'''
 ''' 
 $photosDate YYYY-MM-DD : sends up to three random mars
 photos from a specified date 
